556f157d50f2c1e3f4ece26b679d8848948b1787
Dario Lopez <dalofe@outlook.com>
2025-09-23 17:14:30 +0200

    Day 6-3: improvement suggestions added

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -4,30 +4,31 @@ type TaskFormProps = {
     onAddTask: (title: string) => void;
 }
 
-export default function TaskForm({onAddTask}: TaskFormProps) {
+export default function TaskForm({ onAddTask }: TaskFormProps) {
     const [title, setTitle] = useState<string>("");
-    const [errorMessage, setErrorMessage] = useState<boolean>(false);
+    const [errorMessage, setErrorMessage] = useState<string>("");
 
     const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
         setTitle(event.target.value);
     }
 
-    const handleSubmitChange = (event: FormEvent<HTMLFormElement>) => {
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        if(title.length === 0){
-            setErrorMessage(true);
+        if (title.length === 0) {
+            setErrorMessage("Task title cannot be empty");
         } else {
-            setErrorMessage(false);
+            setErrorMessage("");
             onAddTask(title);
+            setTitle("");
         }
     }
 
     return (
         <div>
             {errorMessage && (<span className="text-red-500 text-sm">Add a task to submit!</span>)}
-            <form onSubmit={handleSubmitChange}>
-                <input type="text" className="border rounded" value={title} onChange={handleInputChange} />
-                <button type="submit">Submit</button>
+            <form onSubmit={handleSubmit}>
+                <input type="text" className="border rounded px-2 py-1" value={title} onChange={handleInputChange} />
+                <button type="submit" className="px-3 py-1 bg-blue-600 text-white rounded hover:bg-blue-700">Submit</button>
             </form>
         </div>
     )
