cd35cb00e00c73b777e9067546e141e0a33bd3fc
Dario Lopez <dalofe@outlook.com>
2025-09-30 15:37:02 +0200

    Day 10: formatting with Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
-import TaskList from "./components/TaskList";
-import type { Task } from "./components/TaskList";
+import { useEffect, useState } from 'react';
+import TaskList from './components/TaskList';
+import type { Task } from './components/TaskList';
 
 export default function App() {
   const loadStates = (key: string, fallback: Task[]): Task[] => {
@@ -8,40 +8,65 @@ export default function App() {
     return saved ? JSON.parse(saved) : fallback;
   };
 
-  const [frontendTasks, setFrontendTasks] = useState<Task[]>(() => loadStates("frontendTasks", [
-    { id: 1, title: "Migrate to React 18", completed: true },
-    { id: 2, title: "Refactor components", completed: true },
-  ]));
+  const [frontendTasks, setFrontendTasks] = useState<Task[]>(() =>
+    loadStates('frontendTasks', [
+      { id: 1, title: 'Migrate to React 18', completed: true },
+      { id: 2, title: 'Refactor components', completed: true },
+    ])
+  );
 
   useEffect(() => {
-    localStorage.setItem("frontendTasks", JSON.stringify(frontendTasks));
+    localStorage.setItem('frontendTasks', JSON.stringify(frontendTasks));
   }, [frontendTasks]);
 
-
-  const [backendTasks, setBackendTasks] = useState<Task[]>(() => loadStates("backendTasks", [
-    { id: 1, title: "Set up Prisma", completed: false },
-    { id: 2, title: "Implement API auth", completed: false },
-  ]));
+  const [backendTasks, setBackendTasks] = useState<Task[]>(() =>
+    loadStates('backendTasks', [
+      { id: 1, title: 'Set up Prisma', completed: false },
+      { id: 2, title: 'Implement API auth', completed: false },
+    ])
+  );
 
   useEffect(() => {
-    localStorage.setItem("backendTasks", JSON.stringify(backendTasks));
+    localStorage.setItem('backendTasks', JSON.stringify(backendTasks));
   }, [backendTasks]);
 
   const handleAddFrotendTasks = (title: string) => {
-    setFrontendTasks(prev => [...prev, {id: Date.now(), title, completed: false}]);
+    setFrontendTasks((prev) => [
+      ...prev,
+      { id: Date.now(), title, completed: false },
+    ]);
   };
 
   const handleAddBackendTasks = (title: string) => {
-    setBackendTasks(prev => [...prev, {id: Date.now(), title, completed: false}]);
+    setBackendTasks((prev) => [
+      ...prev,
+      { id: Date.now(), title, completed: false },
+    ]);
   };
 
   const totalTasks = frontendTasks.length + backendTasks.length;
-  const totalCompleted = frontendTasks.filter(t => t.completed).length + backendTasks.filter(t => t.completed).length;
+  const totalCompleted =
+    frontendTasks.filter((t) => t.completed).length +
+    backendTasks.filter((t) => t.completed).length;
 
   return (
     <div className="p-8 space-y-8">
-      <TaskList title="Frontend Team Tasks" showCompleted={true} tasks={frontendTasks} setTasks={setFrontendTasks} emptyMessage="No frontend tasks" onAddTask={handleAddFrotendTasks}/>
-      <TaskList title="Backend Team Tasks" showCompleted={true} tasks={backendTasks} setTasks={setBackendTasks} emptyMessage="No backend tasks" onAddTask={handleAddBackendTasks} />
+      <TaskList
+        title="Frontend Team Tasks"
+        showCompleted={true}
+        tasks={frontendTasks}
+        setTasks={setFrontendTasks}
+        emptyMessage="No frontend tasks"
+        onAddTask={handleAddFrotendTasks}
+      />
+      <TaskList
+        title="Backend Team Tasks"
+        showCompleted={true}
+        tasks={backendTasks}
+        setTasks={setBackendTasks}
+        emptyMessage="No backend tasks"
+        onAddTask={handleAddBackendTasks}
+      />
       <div className="p-4 bg-gray-100 rounded-lg">
         Overall Progress: {totalCompleted}/{totalTasks} tasks done
       </div>
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,81 +1,113 @@
-import type { Dispatch, SetStateAction } from "react";
-import TaskProgress from "./TaskProgress";
-import TaskForm from "./TaskForm";
+import type { Dispatch, SetStateAction } from 'react';
+import TaskProgress from './TaskProgress';
+import TaskForm from './TaskForm';
 
 export type Task = {
-    id: number;
-    title: string;
-    completed: boolean;
+  id: number;
+  title: string;
+  completed: boolean;
 };
 
 type TaskListProps = {
-    title: string;
-    tasks: Task[];
-    setTasks: Dispatch<SetStateAction<Task[]>>;
-    showCompleted?: boolean;
-    onTaskToggle?: (task: Task) => void;
-    emptyMessage?: string;
-    onAddTask: (title: string) => void;
-}
+  title: string;
+  tasks: Task[];
+  setTasks: Dispatch<SetStateAction<Task[]>>;
+  showCompleted?: boolean;
+  onTaskToggle?: (task: Task) => void;
+  emptyMessage?: string;
+  onAddTask: (title: string) => void;
+};
 
-export default function TaskList({ title, tasks, setTasks, showCompleted = true, onTaskToggle, emptyMessage = "All done here", onAddTask }: TaskListProps) {
-    const toggleTask = (id: number) => {
-        const updatedTasks = tasks.map(task =>
-            task.id === id ? { ...task, completed: !task.completed } : task
-        );
-        setTasks(updatedTasks);
-
-        const toggledTask = updatedTasks.find(task => task.id === id);
-        if (toggledTask && onTaskToggle) {
-            onTaskToggle(toggledTask);
-        }
-    };
-
-    const deleteTask = (id: number) => {
-        const filteredTasksList = tasks.filter(task => task.id !== id);
-        setTasks(filteredTasksList);
-    }
-
-    const emptyList = () => {
-        if (window.confirm("Are you sure you want to clear all the tasks?")) {
-            setTasks([]);
-        }
-    }
-
-    return (
-        <div className="p-6 bg-white rounded-xl shadow-md mt-6">
-            <div className="flex justify-between p-2">
-                <h2 className="text-xl font-semibold mb-4">{title}</h2>
-                <TaskForm onAddTask={onAddTask} />
-            </div>
-            <TaskProgress tasks={tasks} />
-            <ul>
-                {tasks.length === 0 ? (<li className="p-2 text-gray-500 italic">{emptyMessage}</li>) :
-                    tasks
-                        //.filter(task => showCompleted ? true : !task.completed)
-                        .filter(task => showCompleted || !task.completed)
-                        .map(task => (
-                            <li key={task.id} className="flex justify-between items-center p-2 text-gray-800">
-                                <div className="flex items-center gap-2">
-                                    <span className={task.completed ? "line-through text-gray-400" : ""}>{task.title}</span>
-                                    {task.completed && (
-                                        <span className="text-green-600 text-sm">✔</span>
-                                    )}
-                                </div>
-
-                                <div>
-                                    <button onClick={() => toggleTask(task.id)}
-                                        className="px-3 py-1 mr-2 bg-green-500 text-white rounded hover:bg-green-600 transition">{task.completed ? "Undo" : "Done"}</button>
-                                    <button onClick={() => deleteTask(task.id)} className="px-3 py-1 bg-red-600 text-white rounded hover:bg-red-700 transition">x</button>
-                                </div>
-                            </li>
-                        ))
-                }
-            </ul>
-            <div className="grid justify-items-center mt-3">
-                <button onClick={emptyList}
-                    className="px-3 py-1 bg-yellow-200 rounded hover:bg-yellow-300 transition">Clear all tasks</button>
-            </div>
-        </div>
+export default function TaskList({
+  title,
+  tasks,
+  setTasks,
+  showCompleted = true,
+  onTaskToggle,
+  emptyMessage = 'All done here',
+  onAddTask,
+}: TaskListProps) {
+  const toggleTask = (id: number) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === id ? { ...task, completed: !task.completed } : task
     );
+    setTasks(updatedTasks);
+
+    const toggledTask = updatedTasks.find((task) => task.id === id);
+    if (toggledTask && onTaskToggle) {
+      onTaskToggle(toggledTask);
+    }
+  };
+
+  const deleteTask = (id: number) => {
+    const filteredTasksList = tasks.filter((task) => task.id !== id);
+    setTasks(filteredTasksList);
+  };
+
+  const emptyList = () => {
+    if (window.confirm('Are you sure you want to clear all the tasks?')) {
+      setTasks([]);
+    }
+  };
+
+  return (
+    <div className="p-6 bg-white rounded-xl shadow-md mt-6">
+      <div className="flex justify-between p-2">
+        <h2 className="text-xl font-semibold mb-4">{title}</h2>
+        <TaskForm onAddTask={onAddTask} />
+      </div>
+      <TaskProgress tasks={tasks} />
+      <ul>
+        {tasks.length === 0 ? (
+          <li className="p-2 text-gray-500 italic">{emptyMessage}</li>
+        ) : (
+          tasks
+            //.filter(task => showCompleted ? true : !task.completed)
+            .filter((task) => showCompleted || !task.completed)
+            .map((task) => (
+              <li
+                key={task.id}
+                className="flex justify-between items-center p-2 text-gray-800"
+              >
+                <div className="flex items-center gap-2">
+                  <span
+                    className={
+                      task.completed ? 'line-through text-gray-400' : ''
+                    }
+                  >
+                    {task.title}
+                  </span>
+                  {task.completed && (
+                    <span className="text-green-600 text-sm">✔</span>
+                  )}
+                </div>
+
+                <div>
+                  <button
+                    onClick={() => toggleTask(task.id)}
+                    className="px-3 py-1 mr-2 bg-green-500 text-white rounded hover:bg-green-600 transition"
+                  >
+                    {task.completed ? 'Undo' : 'Done'}
+                  </button>
+                  <button
+                    onClick={() => deleteTask(task.id)}
+                    className="px-3 py-1 bg-red-600 text-white rounded hover:bg-red-700 transition"
+                  >
+                    x
+                  </button>
+                </div>
+              </li>
+            ))
+        )}
+      </ul>
+      <div className="grid justify-items-center mt-3">
+        <button
+          onClick={emptyList}
+          className="px-3 py-1 bg-yellow-200 rounded hover:bg-yellow-300 transition"
+        >
+          Clear all tasks
+        </button>
+      </div>
+    </div>
+  );
 }
