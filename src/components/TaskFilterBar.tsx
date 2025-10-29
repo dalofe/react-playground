1d409e2d3d61cbd0ba1549b21cca47983868d0d8
Dario Lopez <dalofe@outlook.com>
2025-10-29 20:22:52 +0100

    Day 21: improving the filterbar tailwind styles

--- src/components/TaskFilterBar.tsx
+++ src/components/TaskFilterBar.tsx
@@ -18,30 +18,38 @@ export function TaskFilterBar({
   numberOfVisibleTasks,
 }: TaskFilterBarProps) {
   return (
-    <div className="flex gap-4 items-center p-3 bg-gray-100 rounded-lg">
-      <div>
-        <label className="mr-2 font-semibold">Filter: </label>
-        <select
-          value={filter}
-          onChange={(e) => onFilterChange(e.target.value as FilterOptions)}
-        >
-          <option value="all">All</option>
-          <option value="completed">Completed</option>
-          <option value="pending">Pending</option>
-        </select>
+    <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 items-center p-3 bg-gray-100 rounded-lg">
+      <div className="flex flex-col sm:flex-row sm:items-center gap-2">
+        <div className="flex items-center gap-2">
+          <label className="mr-2 font-semibold text-sm text-gray-700">
+            Filter:{' '}
+          </label>
+          <select
+            className="border border-gray-300 rounded-md p-1 text-sm"
+            value={filter}
+            onChange={(e) => onFilterChange(e.target.value as FilterOptions)}
+          >
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="pending">Pending</option>
+          </select>
+        </div>
+        <div className="flex items-center gap-2">
+          <label className="mr-2 font-semibold text-sm text-gray-700">
+            Sort:
+          </label>
+          <select
+            className="border border-gray-300 rounded-md p-1 text-sm"
+            value={sort}
+            onChange={(e) => onSortChange(e.target.value as SortOptions)}
+          >
+            <option value="none">None</option>
+            <option value="asc">A-Z</option>
+            <option value="desc">Z-A</option>
+          </select>
+        </div>
       </div>
-      <div>
-        <label className="mr-2 font-semibold">Sort:</label>
-        <select
-          value={sort}
-          onChange={(e) => onSortChange(e.target.value as SortOptions)}
-        >
-          <option value="none">None</option>
-          <option value="asc">A-Z</option>
-          <option value="desc">Z-A</option>
-        </select>
-      </div>
-      <span className="ml-auto text-sm text-gray-600">
+      <span className="ml-auto text-xs sm:text-sm text-right text-gray-600">
         Showing {numberOfVisibleTasks} / {tasks.length} tasks
       </span>
     </div>
